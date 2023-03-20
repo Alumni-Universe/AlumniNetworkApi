e4c1a1540cd64a1917d2577bf94c2c9e6beca6b1
Tartarpaste <tordvg@hotmail.com>
2023-03-20 11:21:29 +0100

    chore: more swagger stuff

--- AlumniNetworkApi/Program.cs
+++ AlumniNetworkApi/Program.cs
@@ -35,12 +35,12 @@ namespace AlumniNetworkApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            /*if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            */
+            
 
             app.UseHttpsRedirection();
 
